b69531debe9972d76cbe05ef06fac351e057f66a
withoutai <iavadiev@yahoo.com>
2020-12-13 13:40:21 +0100

    rl control improvement

--- utils/helper.py
+++ utils/helper.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 
 def print_policy(pi,
@@ -106,3 +107,50 @@ def generate_episode(env, pi):
         obs = new_obs
 
     return episode
+
+
+def linear_decay(start_value, end_value, decay_duration, full_duration):
+    '''
+    Generates a list of float values that decrease from a start_value to the end_value in a linear fashion
+
+    Args:
+        start_value:      The starting value for a float value
+        end_value:        The lowest value that is allowed
+        decay_duration:   Number of episodes the value is decreasing
+        full_duration:    Number of episodes the value is needed
+
+    Returns:
+        decay_values:     Numpy array of decaying values
+    '''
+
+    decay_values = np.arange(decay_duration)[::-1]
+    decay_values = decay_values / np.max(decay_values)
+
+    values_range = start_value - end_value
+    decay_values = decay_values * values_range + end_value
+
+    decay_values = np.pad(
+        decay_values, (0, full_duration - decay_duration), mode='edge')
+
+    return decay_values
+
+
+def epsilon_greedy(Q, state, epsilon):
+    '''
+    Selects an action using epsillon-greedy policy
+
+    Args: 
+        Q:       Action Value function of a policy pi
+        state:   State in the MDP context
+        epsilon: Probability with which a random action is selected
+
+    Returns:
+        Action
+    '''
+
+    if np.random.rand() > epsilon:
+        action = np.argmax(Q[state])
+    else:
+        action = np.random.randint(len(Q[state]))
+
+    return action
