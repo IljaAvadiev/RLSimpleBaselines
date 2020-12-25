f0a2aa2dc3754d82fb1c29769c9bd12098862f6b
withoutai <iavadiev@yahoo.com>
2020-12-25 19:33:27 +0100

    adding ddpg components

--- algorithms/ddpg/model.py
+++ algorithms/ddpg/model.py
@@ -2,22 +2,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import numpy
 
 
 class Value(nn.Module):
     def __init__(self, state_dims, action_dims, hidden_dims, alpha):
         super(Value, self).__init__()
         self.input_layer = nn.Linear(state_dims, hidden_dims[0])
+
         self.hidden_layers = nn.ModuleList()
-        for hidden_dim in range(len(hidden_dims)-1):
-            if hidden_dim == 0:
-                hidden_layer = nn.Linear(
-                    hidden_dims[0] + action_dims, hidden_dims[1])
-            else:
-                hidden_layer = nn.Linear(
-                    hidden_dims[hidden_dim], hidden_dims[hidden_dim+1])
+        input_dims = hidden_dims[0] + action_dims
+        for i in range(len(hidden_dims)-1):
+            hidden_layer = nn.Linear(
+                input_dims, hidden_dims[i+1])
             self.hidden_layers.append(hidden_layer)
+            input_dims = hidden_dims[i+1]
         self.output_layer = nn.Linear(hidden_dims[-1], 1)
 
         self.activation = F.relu
@@ -32,7 +30,7 @@ class Value(nn.Module):
         y = torch.tensor(action, dtype=torch.float32).to(self.device)
 
         x = self.activation(self.input_layer(x))
-        x = self.activation(torch.cat((x, y)))
+        x = torch.cat((x, y), dim=1)
         for hidden_layer in self.hidden_layers:
             x = self.activation(hidden_layer(x))
         x = self.output_layer(x)
@@ -40,7 +38,8 @@ class Value(nn.Module):
 
 
 class Policy(nn.Module):
-    def __init__(self, state_dims, action_dims, hidden_dims, alpha):
+    def __init__(self, state_dims, action_dims, action_ranges, hidden_dims, alpha):
+        super(Policy, self).__init__()
         self.input_layer = nn.Linear(state_dims, hidden_dims[0])
         self.hidden_layers = nn.ModuleList([
             nn.Linear(hidden_dims[i], hidden_dims[i+1]) for i in range(len(hidden_dims)-1)
@@ -53,10 +52,26 @@ class Policy(nn.Module):
             'cuda:0' if torch.cuda.is_available() else 'cpu')
         self.optimizer = optim.Adam(self.parameters(), lr=alpha)
 
+        # prepare these variables for rescaling
+        self.min = torch.tanh(torch.tensor(
+            [float('-inf')], dtype=torch.float32)).to(self.device)
+        self.max = torch.tanh(torch.tensor(
+            [float('inf')], dtype=torch.float32)).to(self.device)
+        self.action_min = torch.tensor(
+            action_ranges[0], dtype=torch.float32).to(self.device)
+        self.action_max = torch.tensor(
+            action_ranges[1], dtype=torch.float32).to(self.device)
+
     def forward(self, state):
-        x = torch.tensor(state, dtype=float32).to(self.device)
+        x = torch.tensor(state, dtype=torch.float32).to(self.device)
         x = self.activation(self.input_layer(x))
         for hidden_layer in self.hidden_layers:
             x = self.activation(hidden_layer(x))
-        x = F.tanh(self.output_layer(x))
-        return x
+        x = torch.tanh(self.output_layer(x))
+        return self.rescale(x)
+
+    def rescale(self, x):
+        # use the following rescaling function
+        # scale from [min, max] to [action_min, action_max]
+        # f(x) = ((action_max - action_min) * (x - min)) / (max - min) + action_min
+        return ((self.action_max - self.action_min) * (x - self.min)) / (self.max - self.min) + self.action_min
