aec90716f30f97c6f74b618ca82f32da76635816
withoutai <iavadiev@yahoo.com>
2020-12-29 15:01:37 +0100

    starting sac

---
+++ algorithms/sac/agent.py
@@ -0,0 +1,12 @@
+class Agent():
+    def __init__(self, state_dims, action_dims, hidden_dims, lr_actor, lr_critic, gamma, tau, mem_size, batch_size, log_sigma_min, log_sigma_max, warmup):
+        pass
+
+    def store_transition(self):
+        pass
+
+    def learn(self):
+        pass
+
+    def update_target_weights(self):
+        pass
