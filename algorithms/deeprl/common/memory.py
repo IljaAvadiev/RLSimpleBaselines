ce6de91e9a235dc70913f39ba9c8a5233f1d521c
withoutai <iavadiev@yahoo.com>
2021-01-07 07:50:00 +0100

    small bug fixes

--- algorithms/deeprl/common/memory.py
+++ algorithms/deeprl/common/memory.py
@@ -8,7 +8,7 @@ class ReplayBuffer():
 
     def __init__(self, state_dims, action_dims, max_memory_size, batch_size):
         self.batch_size = batch_size
-        self.memory_size = memory_size
+        self.max_memory_size = max_memory_size
         self.idx = 0
         self.current_memory_size = 0
 
@@ -19,14 +19,14 @@ class ReplayBuffer():
         self.next_states = np.empty(
             shape=(max_memory_size, state_dims), dtype=np.float32)
         self.rewards = np.empty(shape=(max_memory_size, 1), dtype=np.float32)
-        self.terminals = np.empty(shape(max_memory_size, 1), dtype=np.bool)
+        self.terminals = np.empty(shape=(max_memory_size, 1), dtype=np.bool)
 
     def add_memory(self, state, action, next_state, reward, done):
-        self.states[idx] = state
-        self.actions[idx] = action
-        self.next_states[idx] = next_state
-        self.rewards[idx] = reward
-        self.terminals[idx] = done
+        self.states[self.idx] = state
+        self.actions[self.idx] = action
+        self.next_states[self.idx] = next_state
+        self.rewards[self.idx] = reward
+        self.terminals[self.idx] = done
 
         self.idx = (self.idx + 1) % self.max_memory_size
         self.current_memory_size = min(
